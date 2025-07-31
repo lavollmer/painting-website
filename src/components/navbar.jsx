ab56ffad52b5df8015f3819d71d41f7b52610756
Laura <laura.e.vollmer@gmail.com>
2025-07-31 12:51:41 -0500

    Installed React-Router-Dom successfully and added homepage component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,16 @@ import '../App.css'
 
 const navbar = () => {
     return (
-        <div className='flex flex-row'>
-            <h1>Logo</h1>
-            <h2>Navigation</h2>
+        <div className='flex flex-row space-evenly'>
+            <div className='flex flex-row'>
+                <h1>Logo</h1>
+            </div>
+            <div className='flex flex-row space-x-4'>
+                <h2>About</h2>
+                <h2>Pricing</h2>
+                <h2>Inquires</h2>
+                <h2>Contact</h2>
+            </div>
         </div>
     )
 }
