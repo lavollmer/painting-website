e43ddd9b6c80b25e29c983575ac1ecc471e7749e
Laura <laura.e.vollmer@gmail.com>
2025-07-31 12:45:25 -0500

    Debugging TailwindCSS

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div className='flex flex-row'>
+            <h1>Logo</h1>
+            <h2>Navigation</h2>
+        </div>
+    )
+}
+
+export default navbar
