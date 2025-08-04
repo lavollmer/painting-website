6136b1b5d59ec40a5686317cb7f3e64a906c73fd
Laura <laura.e.vollmer@gmail.com>
2025-08-04 16:35:20 -0500

    Changing the navbar

--- src/components/inquiries.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const inquiries = () => {
-  return (
-    <div>inquiries</div>
-  )
-}
-
-export default inquiries
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,14 @@ const navbar = () => {
     return (
         <div className='flex flex-row space-x-10 p-10 items-stretch content-center bg-black text-white w-100%'>
             <div className='flex flex-column space-x-2 font-sans font-bold'>
-                <h1 className='text-lg'>Homes & Halls By Laura</h1>
+                <h1 className='text-lg'>Painted Places By Laura</h1>
             </div>
             <div className='flex flex-row space-x-4'>
+                <h2>Shop</h2>
+                <h2>Commissions</h2>
+                <h2>Gallery</h2>
                 <h2>About</h2>
-                <h2>Pricing</h2>
-                <h2>Inquires</h2>
+                <h2>FAQ</h2>
                 <h2>Contact</h2>
             </div>
         </div>
