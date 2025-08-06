5d2e9755a600b5def8c3876b768b80a72182d694
Laura <laura.e.vollmer@gmail.com>
2025-08-06 11:53:16 -0500

    Updating the homepage

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,17 +2,31 @@ import '../App.css'
 import '../index.css'
 import Navbar from './navbar'
 import Footer from "./footer"
+import {Flex, Heading, Box, Text} from '@chakra-ui/react'
 
 const homepage = () => {
-  return (
-    <div className='flex'>
-      <Navbar />
-      <h1 className="text-3xl font-bold text-red-500">Hello Tailwind</h1>
-      <h1 className='bg-cherry-pink text-white p-4'>Custom Watercolor Illustrations</h1>
-      <h1>By Laura</h1>
-      <Footer/>
-    </div>
-  )
+return (
+  <Flex direction="column">
+    <Navbar />
+
+    <Heading as="h1" size="xl" color="red.500" fontWeight="bold" mb={4}>
+      Hello Chakra
+    </Heading>
+
+    <Box bg="cherryPink" color="white" p={4} mb={4}>
+      <Heading as="h1" size="md">
+        Custom Watercolor Illustrations
+      </Heading>
+    </Box>
+
+    <Text fontSize="lg" mb={4}>
+      By Laura
+    </Text>
+
+    <Footer />
+  </Flex>
+);
+
 }
 
 export default homepage
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import App from './App.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </ChakraProvider>
 )
